d7b29e853d4cf33df041a17c3df419a04d458107
Konstantin Bukin <bukin@inetstudio.ru>
2020-11-18 19:43:13 +0300

    Add url to brandcash service

--- package/config/services.php
+++ package/config/services.php
@@ -2,7 +2,8 @@
 
 return [
     'brand_cash' => [
-        'api_key'  => env('BRAND_CASH_API_KEY'),
+        'api_key' => env('BRAND_CASH_API_KEY'),
+        'url' => env('BRAND_CASH_URL'),
     ],
     'fns' => [
         'api_key'  => env('FNS_API_KEY'),
